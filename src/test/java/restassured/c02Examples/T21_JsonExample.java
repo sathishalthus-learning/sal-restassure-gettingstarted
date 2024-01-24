87a7f95fc4c43bdb58f9ed8ad7466fdb86964bc5
sathishalthus-learning <sathishalthus.learning@gmail.com>
2024-01-24 15:59:58 +0530

    SomeCommit

--- src/test/java/restassured/c02Examples/T21_JsonExample.java
+++ src/test/java/restassured/c02Examples/T21_JsonExample.java
@@ -6,80 +6,77 @@ import static org.hamcrest.Matchers.*;
 
 import org.junit.Test;
 
+import io.restassured.RestAssured;
+import io.restassured.config.JsonConfig;
+import io.restassured.path.json.config.JsonPathConfig.NumberReturnType;
+
 public class T21_JsonExample {
 	
 //	1.	
-//	---------------------------------------
+////	---------------------------------------
 //	@Test()
-//	public void jsonExampleEqualTo() {
-//	given()
+//	public void json1_EqualTo() {
+//	
+//		given()
 //			//
 //		.when()
-//			.get("https://petstore.swagger.io/v2/pet/9")
+//			.get("http://localhost:3000/lotto")
 //		.then()
-////			.body("id", equalTo(9))
-//			.assertThat().body("id", equalTo(9))
+//			.log().body()
+////			.body("lottoId", equalTo(5))
+//			.assertThat().body("lottoId", equalTo(5))
 //		;
 //	}
 
 //	2.
 //	---------------------------------------
 //	@Test()
-//	public void jsonExampleHasItems() {
+//	public void json2_HasItems() {
 //		given()
 //			//
 //		.when()
-//			.get("https://petstore.swagger.io/v2/pet/findByStatus?status=available")
+//			.get("http://localhost:3000/lotto")
 //		.then()
-////			.body("id", hasItems(9))
-//			.assertThat().body("id", hasItems(9))
+////			.body("winners.winnerId", hasItems(23,54))
+//			.assertThat().body("winners.winnerId", hasItems(23,54))
 //		;
 //	}
 	
 //	3.
 //	---------------------------------------
 //	@Test()
-//	public void jsonFloatsDoublesAsBiddeicimal() {
-//		get("/price").then().body("price", is(12.12f));
-//	
-//		given().
-//    		config(RestAssured.config().jsonConfig(jsonConfig().numberReturnType(BIG_DECIMAL))).
-//		when().
-//    		get("/price").
-//		then().
-//    		body("price", is(new BigDecimal(12.12));
+//	public void json3_FloatsDoubles() {
+//		given()
+//			//
+//		.when()
+//			.get("http://localhost:3000/store")
+//		.then()
+//			.body("book[0].price",is(8.95f))
+//		;
 //	}
 	
-	
-//	3.
+//	4.
 //	---------------------------------------
 //	@Test()
-//	public void jsonSchemaValidation() {
-//
+//	public void json4_FloatsDoublesAsBiddeicimal() {
+//		given()
+//			.config(RestAssured.config().jsonConfig(JsonConfig.jsonConfig().numberReturnType(NumberReturnType.BIG_DECIMAL)))
+//		.when()
+//			.get("http://localhost:3000/store")
+//		.then()
+//			.body("book[0].price",is(new BigDecimal(12.12)))
+//		;
 //	}
-//	
+	
+//	5.---------------------------------------
 //	@Test()
-//	public void xmlExample() {
-//
-//	}
-//	
-//	@Test()
-//	public void advancedXML() {
-//
-//	}
-//	
-//	@Test()
-//	public void advancedJSON() {
-//
-//	}
-//	
-//	@Test()
-//	public void deSerializationWithGenereics() {
-//
-//	}
-//	
-//	@Test()
-//	public void additionalExamples() {
-//
+//	public void json5_schemaValidation() {
+//		given()
+//			.config(RestAssured.config().jsonConfig(JsonConfig.jsonConfig().numberReturnType(NumberReturnType.BIG_DECIMAL)))
+//		.when()
+//			.get("http://localhost:3000/store")
+//		.then()
+//			.body("book[0].price",is(new BigDecimal(12.12)))
+//		;
 //	}
 }
---
+++ src/test/java/restassured/c02Examples/T24a_AdvancedXml.java
@@ -0,0 +1,85 @@
+package restassured.c02Examples;
+
+import static io.restassured.RestAssured.*;
+//import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+import org.junit.Test;
+
+public class T24a_AdvancedXml {
+	
+//	1.	
+//	---------------------------------------
+//	@Test()
+//	public void jsonExampleEqualTo() {
+//	given()
+//			//
+//		.when()
+//			.get("https://petstore.swagger.io/v2/pet/9")
+//		.then()
+////			.body("id", equalTo(9))
+//			.assertThat().body("id", equalTo(9))
+//		;
+//	}
+
+//	2.
+//	---------------------------------------
+//	@Test()
+//	public void jsonExampleHasItems() {
+//		given()
+//			//
+//		.when()
+//			.get("https://petstore.swagger.io/v2/pet/findByStatus?status=available")
+//		.then()
+////			.body("id", hasItems(9))
+//			.assertThat().body("id", hasItems(9))
+//		;
+//	}
+	
+//	3.
+//	---------------------------------------
+//	@Test()
+//	public void jsonFloatsDoublesAsBiddeicimal() {
+//		get("/price").then().body("price", is(12.12f));
+//	
+//		given().
+//    		config(RestAssured.config().jsonConfig(jsonConfig().numberReturnType(BIG_DECIMAL))).
+//		when().
+//    		get("/price").
+//		then().
+//    		body("price", is(new BigDecimal(12.12));
+//	}
+	
+	
+//	3.
+//	---------------------------------------
+//	@Test()
+//	public void jsonSchemaValidation() {
+//
+//	}
+//	
+//	@Test()
+//	public void xmlExample() {
+//
+//	}
+//	
+//	@Test()
+//	public void advancedXML() {
+//
+//	}
+//	
+//	@Test()
+//	public void advancedJSON() {
+//
+//	}
+//	
+//	@Test()
+//	public void deSerializationWithGenereics() {
+//
+//	}
+//	
+//	@Test()
+//	public void additionalExamples() {
+//
+//	}
+}
