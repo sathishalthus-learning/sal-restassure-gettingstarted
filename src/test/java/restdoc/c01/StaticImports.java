351464d43206bb8b290444a0d58d0d7224942b96
sathishalthus-learning <sathishalthus.learning@gmail.com>
2024-01-24 17:04:00 +0530

    Chapter 1 updated.

---
+++ src/test/java/restdoc/c01/StaticImports.java
@@ -0,0 +1,20 @@
+package restdoc.c01;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+import static io.restassured.module.jsv.JsonSchemaValidator.*;
+
+public class StaticImports {
+	//
+	public void staticImports() {
+		//
+		given()
+		.when()
+		.then()
+		;
+	}
+	
+	
+
+}
